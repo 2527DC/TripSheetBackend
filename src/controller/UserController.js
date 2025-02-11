e25f36edb4137b2f84d7dcb3809e4ccb924bd51b
Chethan <chethan962089@gmail.com>
2025-02-11 12:48:34 +0530

    check

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -49,6 +49,7 @@ export const addTripSheet = async (req, res) => {
       parkingCharges, 
       toolCharges 
     } = req.body;
+console.log(" this is the driver request " ,req.body);
 
     // ✅ Step 1: Validate Required Fields
     if (!Driversignature || !Guestsignature || !openKm || !openHr || !closeKm || !closeHr || !totalKm || !formId) {
